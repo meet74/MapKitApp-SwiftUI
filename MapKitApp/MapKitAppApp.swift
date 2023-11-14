d2c9015485572742cc7eb5c8324c5f6ac1488fdd
meet74 <meet237@gmail.com>
2023-11-14 14:01:19 -0500

    Initial Commit

---
+++ MapKitApp/MapKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAppApp.swift
+//  MapKitApp
+//
+//  Created by Meet Patel on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct MapKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
